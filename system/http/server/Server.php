3b236e729d37dc5d34240e87e62d21a26cbb30df
photon.dev@yandex.ru <Ti5vKD3P1JE56y6S>
2021-02-14 12:37:47 +0300

    new class server

---
+++ system/http/server/Server.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+/**
+ * NomiCMS - Content Management System
+ *
+ * @author Tosyk, Photon
+ * @package nomicms/NomiCMS
+ * @link   http://nomicms.ru
+ */
+
+namespace System\Http\Cookie;
+
+/**
+ * Класс Server
+ */
+class Server
+{
+}
