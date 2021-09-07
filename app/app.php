c9d368c951e764352daaee5836281ac842825934
Denis <63118055+Photon-Dev@users.noreply.github.com>
2021-09-07 21:00:57 +0300

    new file

---
+++ app/app.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * NomiCMS - Content Management System
+ *
+ * @author Tosyk, Photon
+ * @package nomicms/NomiCMS
+ * @link   http://nomicms.ru
+ */
+
+// Использовать
+use System\Container\Container;
+use System\App\App;
