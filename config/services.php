9e4e19ccea146d42b7c78b8ee08546b52c56d292
Denis <63118055+Photon-Dev@users.noreply.github.com>
2021-09-07 21:11:57 +0300

    edit to services

--- config/services.php
+++ config/services.php
@@ -13,37 +13,26 @@
 return [
     // Config
     System\Config\Config::class,
-
      // Json
      System\Json\Json::class,
-
      // Config
      System\Router\Router::class,
-
      // Database
      System\Database\DB::class,
-
      // User
      Packages\User\Component\User::class,
-
      // Session
      System\Http\Session\Session::class,
-
      // Cookie
      System\Http\Cookie\Cookie::class,
-
      // Request
      System\Http\Request\Request::class,
-
      // Response
      System\Http\Response\Response::class,
-
      // View
      System\View\View::class,
-
      // Themes
      Packages\Themes\Component\Themes::class,
-
      // Pagination
      System\Text\Pagination::class
 ];
