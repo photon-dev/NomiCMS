077cf4f20bc34863b02c13000d2f9b511172094d
Denis <photon.dev@yandex.ru>
2021-09-16 14:49:55 +0300

    edit sql files

--- database/news_comments.sql
+++ database/news_comments.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `news_comments` (
     `user_uid` int(10) unsigned NOT NULL,
     `message` text NOT NULL,
     `date_write` int(10) NOT NULL,
-    `date_edit` int(10) DEFAULT NOT NULL
+    `date_edit` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci AUTO_INCREMENT=2 COMMENT='Комментарии новостей';
 
 --
@@ -32,4 +32,3 @@ ALTER TABLE `news_comments`
 
 INSERT INTO `news_comments` (`uid`, `news_uid`, `user_uid`, `message`, `date_write`, `date_edit`) VALUES
 (1, 1, 1, 'Ура. Ура. Ура', 1613296024, 1613296024);
-COMMIT;
