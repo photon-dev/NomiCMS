fa5b51b695f7eb83f7798a94ad4f5815de08bbce
Denis <photon.dev@yandex.ru>
2021-09-20 13:57:42 +0300

    edit

--- web/run.php
+++ web/run.php
@@ -21,13 +21,4 @@ define('NOMI_MEMORY', memory_get_usage());
 $app = require ROOT . 'system/bootstrap.php';
 
 // Запустить приложение
-//echo $app;
 $app->run();
-
-// Создано на момент тестирования
-//echo '<br />' . $app->run();
-/*
-echo '<br /><br />Память: ' . round((memory_get_usage() - NOMI_MEMORY) / 1024) . ' кб';
-echo '<br />Загрузчик: ' . $autoload->getCounter() . ' за ' . round($autoload->getTiming(), 6);
-echo '<br />Генерация: ' . round(microtime(true) - NOMI_START, 6);
-*/
