975b32c5c63efa728872183a74ee1546884e0632
Denis <photon.dev@yandex.ru>
2021-09-27 15:38:19 +0300

    add emoji

---
+++ packages/info/emoji.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * NomiCMS - Content Management System
+ *
+ * @author Tosyk, Photon
+ * @package nomicms/NomiCMS
+ * @link   http://nomicms.ru
+ */
+
+/**
+ * Список эмодзи
+ */
+return [
+    [
+        'tag' => [':-)', ':)']
+    ],
+	'sad' => '\1F614', /* Грусть */
+    'grin' => '\1F600' /* Ухмылка */
+];
---
+++ system/text/Emoji.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * NomiCMS - Content Management System
+ *
+ * @author Tosyk, Photon
+ * @package nomicms/NomiCMS
+ * @link   http://nomicms.ru
+ */
+
+namespace System\Text;
+
+// Использовать
+use System\Container\ContainerInterface;
+use System\View\TemplateInteface;
+
+/**
+ * Класс Emoji
+ */
+class Emoji
+{
+    public static function emo(string $text)
+    {
+        return $text;
+    }
+}
