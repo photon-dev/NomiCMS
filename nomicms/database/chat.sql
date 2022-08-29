b82a42b538cf49c2af045a7b53e2e6316a6a7c15
Denis <photon.dev@yandex.ru>
2022-08-29 14:52:03 +0300

    update

--- nomicms/database/chat.sql
+++ nomicms/database/chat.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS `chat` (
     `uid` int(10) unsigned NOT NULL,
     `user_uid` int(10) NOT NULL,
     `message` text NOT NULL,
-    `date_write` int(10) NOT NULL
+    `date_write` int(10) NOT NULL,
+    `date_edit` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_general_ci AUTO_INCREMENT=3 COMMENT='Мини-чат';
 
 --
@@ -25,4 +26,4 @@ ALTER TABLE `chat`
 
 INSERT INTO `chat` (`uid`, `user_uid`, `message`, `date_write`) VALUES
 (1, 1, '[b]Тестирование ютуба[/b]\r\n\r\n[youtube]https://youtu.be/8PCfDOB7KbQ[/youtube]', 1632578817),
-(2, 1, '[b]Тестирование смайлов[/b]\r\n\r\n:smile: :smile2: :smile3: :smile4: :smile5: :smileup: :smileangel: :wink:\r\n\r\n:grin: :grin2: :biggrin: :biggrin2: :lol: :lol2::lol3:\r\n\r\n:kissyes: :kissyes2: :kissyes3: :kisslove: :love: :love2: :lovestar:\r\n\r\n:think: :hugs: :qui:\r\n\r\n:tab: :tab2: :tab3: :tab4: :tab5: :tab6: :sad:\r\n\r\n:virus: :fire: :sssr:\r\n', 1632582310);
+(2, 1, '[b]Тестирование смайлов[/b]\r\n\r\n:smile: :smile2: :smile3
