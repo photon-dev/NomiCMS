c92a1872050885cfe2cb8ec4a0a92d4fe7d2acdf
Denis <photon.dev@yandex.ru>
2023-03-26 15:17:38 +0300

    new config file smailes

---
+++ nomicms/config/smiles.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * NomiCMS - Content Management System
+ *
+ * @author Tosyk, Photon
+ * @package nomicms/NomiCMS
+ * @link   http:',//nomicms.ru
+ */
+
+ /*
+  * Смайлы
+  */
+return [
+    'smile',
+    /***** Улыбающиеся лица *****/
+    'smile',
+    'smile2',
+    'smile3',
+    'smile4',
+    'smile5',
+    'smileup',
+    'smileangel',
+    'wink',
+     /* Большая улыбка */
+    'grin',
+    'grin2',
+    'biggrin',
+    'biggrin2',
+     /* Смех */
+     'lol',
+     'lol2',
+     'lol3',
+    /***** Влюбленное лицо *****/
+    'kissyes',
+    'kissyes2',
+    'kissyes3',
+    'kisslove',
+    'love',
+    'love2',
+    'lovestar',
+    /***** Лицо с рукой *****/
+    'think',
+    'hugs',
+    'qui',
+    /* Язык */
+    'tab',
+    'tab2',
+    'tab3',
+    'tab4',
+    'tab5',
+    'tab6',
+    'sad',
+    /***** Другие *****/
+    'virus',
+    'fire',
+    'sssr'
+];
